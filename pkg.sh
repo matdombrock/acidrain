f60be51bc17195ba419caf13e38f2cfbd76ad5e1
matdombrock <dombrock.mathieu@gmail.com>
2026-01-09 02:13:26 -0800

    wasm-strip

--- pkg.sh
+++ pkg.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# NOTE: Requires `wasm-strip` from `wabt` package.
+# https://github.com/WebAssembly/wabt/releases
+
 set -e
 title="ACIDRAIN"
 
 cargo build --release
+wasm-strip target/wasm32-unknown-unknown/release/cart.wasm
 cd target/wasm32-unknown-unknown/release
 w4 bundle cart.wasm --title "$title" --html index.html \
   --windows $title-windows.exe \
@@ -26,3 +30,5 @@ zip -j "$out_dir_web.zip" $out_dir_web/*
 cp $target/"$title"-windows.exe $out_dir
 cp $target/"$title"-macos $out_dir
 cp $target/"$title"-linux $out_dir 
+
+echo "Packaged builds are in the '$out_dir' directory."
